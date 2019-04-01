80de018daab9cb786e62aece99a5bc83ca4cd2dd
alexkessinger <alexkess101@gmail.com>
2019-04-01 13:07:14 -0600

    tables finished

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_heroku import Heroku
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -13,32 +14,78 @@ heroku = Heroku(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     __tablename__="user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    current_sales = db.Column(db.Integer, nullable=False)
     num_sales_goal = db.Column(db.Integer, nullable=False)
+    income_current = db.Column(db.Integer, nullable=False)
     income_total = db.Column(db.Integer, nullable=False)
     commission_percentage = db.Column(db.Float, nullable=False)
-    signing_bonuses = db.Column(db.Integer, nullable=True)
-    incentives = db.Column(db.Integer, nullable=True)
-    expenses = db.Column(db.Integer, nullable=True)
-    
+    expenseHistory = db.relationship('ExpenseHistory', backref='user', lazy=True)
+    saleHistory = db.relationship('SaleHistory', backref='user', lazy=True)
+    incentiveHistory = db.relationship('IncentiveHistory', backref='user', lazy=True)
 
-    def __init__(self, email, password, num_sales_goal, income_total, commission_percentage, signing_bonuses, incentives, expenses):
+    def __init__(self, email, password, current_sales, num_sales_goal, income_current, income_total, commission_percentage):
         self.email = email
         self.password = password
+        self.current_sales = current_sales
         self.num_sales_goal = num_sales_goal
+        self.income_current = income_current
         self.income_total = income_total
         self.commission_percentage = commission_percentage
-        self.signing_bonuses = signing_bonuses
-        self.incentives = incentives
-        self.expenses = expenses
+        
+        
 
     def __repr__(self):
         return '<Title %r>' % self.title
 
+
+class ExpenseHistory(db.Model):
+    __tablename__="expenseHistory"
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+    expense = db.Column(db.String(250), nullable=False)
+    expense_amount = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, date_created, expense, expense_amount, user_id):
+        self.date_created = date_created
+        self.expense = expense
+        self.expense_amount == expense_amount
+        self.user_id = user_id
+
+class SaleHistory(db.Model):
+    __tablename__="saleHistory"
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+    account_name_first = db.Column(db.String(150), nullable=False)
+    account_name_last = db.Column(db.String(150), nullable=False)
+    account_value = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, account_name_first, account_name_last, account_value, user_id):
+        self.account_name_first = account_name_first
+        self.account_name_last = account_name_last
+        self.account_value = account_value
+        self.user_id = user_id
+
+class IncentiveHistory(db.Model):
+    __tablename__='incentiveHistory'
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, server_default=db.func.now())
+    incentive_name = db.Column(db.String(150), nullable=False)
+    incentive_value = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, date_created, incentive_name, incentive_value, user_id):
+        self.date_created = date_created
+        self.incentive_name = incentive_name
+        self.incentive_value = incentive_value
+        self.user_id = user_id
 
 
 
@@ -54,14 +101,13 @@ def User_input():
         post_data = request.get_json()
         email = post_data.get('email')
         password = post_data.get('password')
+        current_sales = post_data.get('current_sales')
         num_sales_goal = post_data.get('num_sales_goal')
-        commission_percentage = post_data.get('commission_percentage')
-        signing_bonuses = post_data.get('signing_bonuses')
-        incentives = post_data.get('incentives')
+        income_current = post_data.get('income_total')
         income_total = post_data.get('income_total')
-        expenses = post_data.get('expenses')
-
-        user = User(email, password, num_sales_goal, commission_percentage, signing_bonuses, incentives, income_total, expenses)
+        commission_percentage = post_data.get('commission_percentage')
+    
+        user = User(email, password, current_sales, num_sales_goal, income_current, income_total, commission_percentage)
         db.session.add(user)
         db.session.commit()
         return jsonify("Everything has worked correctly")
@@ -70,7 +116,7 @@ def User_input():
 
 @app.route('/login', methods=['GET'])
 def get_users():
-    users = db.session.query(User.id, User.email, User.password, User.num_sales_goal, User.commission_percentage, User.signing_bonuses, User.incentives, User.income_total, User.expenses).all()
+    users = db.session.query(User.id, User.email, User.password, User.current_sales, User.num_sales_goal, User.income_current, User.income_total, User.commission_percentage).all()
     return jsonify(users)
 
 @app.route('/user/delete/<id>', methods=['DELETE'])
@@ -79,6 +125,37 @@ def delete_user(id):
     db.session.delete(user)
     db.session.commit()
     return jsonify("User deleted")
+
+
+@app.route('/sale/create', methods=['POST'])
+def post_sale():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        account_name_first = post_data.get('account_name_first')
+        account_name_last = post_data.get('account_name_last')
+        account_value = post_data.get('account_value')
+        user_id = post_data.get('user_id')
+
+        reg = SaleHistory(account_name_first, account_name_last, account_value, user_id)
+        db.session.add(reg)
+        db.session.commit()
+        return jsonify("Everything has worked correctly")
+    return jsonify("Something went wrong with adding a sale")
+
+@app.route('/sale/<id>', methods=['GET'])
+def get_sale(id):
+    sale = db.session.query(SaleHistory.id, SaleHistory.date_created, SaleHistory.account_name_first, SaleHistory.account_name_last, SaleHistory.account_value).filter(SaleHistory.user_id==id).order_by(SaleHistory.date_created).all()
+    return jsonify(sale)
+
+
+@app.route('/sale', methods=['GET'])
+def get_all_sale():
+    sale = db.session.query(SaleHistory.id, SaleHistory.date_created, SaleHistory.account_name_first, SaleHistory.account_name_last, SaleHistory.account_value, SaleHistory.user_id).all()
+    return jsonify(sale)
+
+
+
+
 
 
         
